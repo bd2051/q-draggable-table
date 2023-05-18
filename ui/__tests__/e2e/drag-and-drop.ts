82c7a001581ba21ccc2adedc693a464a2be3fd0e
bd2051 <bd2051@mail.ru>
2023-05-18 17:15:36 +0300

    fix e2e test

--- ui/__tests__/e2e/drag-and-drop.ts
+++ ui/__tests__/e2e/drag-and-drop.ts
@@ -2,19 +2,22 @@ import { Locator, Page } from 'playwright';
 
 const dragAndDrop = async (
   page: Page,
-  TestElement: Locator,
+  testElement: Locator,
   dragCellCaption: string,
   dropCellCaption: string,
 ) => {
-  await TestElement
+  const target = testElement.getByRole('cell', { name: dropCellCaption });
+  await target.hover();
+  await testElement
     .getByRole('cell', { name: dragCellCaption })
     .hover();
-  await page.mouse.down();
-  const target = TestElement
-    .getByRole('cell', { name: dropCellCaption });
   const targetBox = await target.boundingBox();
-  await page.mouse.move(targetBox.x + targetBox.width / 2, targetBox.y + targetBox.height / 2);
-  await target.hover();
+  const x = targetBox.x + targetBox.width / 2;
+  const y = targetBox.y + targetBox.height / 2;
+  await page.mouse.down();
+  await page.mouse.move(x, y, {
+    steps: 20,
+  });
   await page.mouse.up();
 };
 export default dragAndDrop;
